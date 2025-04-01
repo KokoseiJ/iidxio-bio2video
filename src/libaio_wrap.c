58b45183ab9361989cb8645296646ffb75285001
Wonjun Jung <kokoseij@gmail.com>
2025-03-31 17:38:28 -0700

    libaio: oops forgot to add the label...

--- src/libaio_wrap.c
+++ src/libaio_wrap.c
@@ -1,17 +1,26 @@
 #include "libaio_wrap.h"
 
 void *init_bio2() {
-	void *sciUsbCdc = aioIob2Bi2x_OpenSciUsbCdc(0);
+	void *sciUsbCdc, *aioNMgrIob2, *Bi2xTdj;
+	aioNMgrIob_BeginManage_t aioNMgrIob_BeginManage;
+
+	sciUsbCdc = aioIob2Bi2x_OpenSciUsbCdc(0);
 	if (!sciUsbCdc) goto error;
 	
-	void *aioNMgrIob2 = aioNMgrIob2_Create(sciUsbCdc, 0);
+	aioNMgrIob2 = aioNMgrIob2_Create(sciUsbCdc, 0);
 	if (!aioNMgrIob2) goto error;
 
-	void *bi2xTdj = aioIob2Bi2xTDJ_Create(aioNMgrIob2, 1);
+	bi2xTdj = aioIob2Bi2xTDJ_Create(aioNMgrIob2, 1);
+	if (!bi2xTdj) goto error;
 
-	aioNMgrIob_BeginManage_t aioNMgrIob_BeginManage = (aioNMgrIob2 + 80);
+	aioNMgrIob_BeginManage = (aioNMgrIob2 + 80);
+	if (!aioNMgrIob_BeginManage) goto error;
+
 	aioNMgrIob_BeginManage(aioNMgrIob2);
 
 	return bi2xTdj;
+
+error:
+	return NULL;
 }
 
