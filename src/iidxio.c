343bfd1be034ec24071da9d367b6fdf2349a02e2
Wonjun Jung <kokoseij@gmail.com>
2025-03-31 17:18:42 -0700

    iidxio: implement bi2x initialization and light updates

--- src/iidxio.c
+++ src/iidxio.c
@@ -1,48 +1,107 @@
-#include <stdbool.h>
-#include <stdint.h>
+#include "libaio_wrap.h"
 #include "bemanitools/glue.h"
 #include "bemanitools/iidxio.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+#define MODULE "iidxio-bio2video"
+
+log_formatter_t misc, info, warning, fatal;
+
+thread_create_t thread_create;
+thread_join_t thread_join;
+thread_destroy_t thread_destroy;
+
+void *bi2x = NULL;
+
+uint8_t bi2x_inputBuffer[202];
 
 void iidx_io_set_loggers(
-    log_formatter_t misc,
-    log_formatter_t info,
-    log_formatter_t warning,
-    log_formatter_t fatal) {
+    log_formatter_t p_misc,
+    log_formatter_t p_info,
+    log_formatter_t p_warning,
+    log_formatter_t p_fatal)
+{
+	misc = p_misc;
+	info = p_info;
+	warning = p_warning;
+	fatal = p_fatal;
+
 	return;
 }
 
 bool iidx_io_init(
-    thread_create_t thread_create,
-    thread_join_t thread_join,
-    thread_destroy_t thread_destroy) {
+    thread_create_t p_thread_create,
+    thread_join_t p_thread_join,
+    thread_destroy_t p_thread_destroy)
+{
+	thread_create = p_thread_create;
+	thread_join_t = p_thread_join;
+	thread_destroy_t = p_thread_destroy;
+
+	info(MODULE, "Hello, World!");
+
+	bi2x = init_bio2();
+	if (!bi2x) {
+		fatal(MODULE, "Failed to initialize BI2X");
+		return false;
+	}
+
+	info(MODULE, "BI2X device initialized");
 	return true;
 }
 
 void iidx_io_fini(void) {
+	// TODO: do something...
 	return;
 }
 
 /* Set the deck lighting state. See enum iidx_io_deck_light above. */
 
 void iidx_io_ep1_set_deck_lights(uint16_t deck_lights) {
+	int player_side, button_idx;
+
+	for (player_side=0; player_side<2; player_side++) {
+		for (button_idx=0; button_idx<7; button_idx++) {
+			aioIob2Bi2xTDJ_SetPlayerButtonLamp(
+				bi2x, player_side, button_idx,
+				deck_lights >> (player_side * 8 + button_idx) & 1
+			);
+		}
+	}
 	return;
 }
 
 /* Set front panel lighting state. See enum iidx_io_panel_light above. */
 
 void iidx_io_ep1_set_panel_lights(uint8_t panel_lights) {
+	aioIob2Bi2xTDJ_SetStartLamp(
+		bi2x, 0, panel_lights >> IIDX_IO_PANEL_LIGHT_P1_START & 1
+	);
+	aioIob2Bi2xTDJ_SetStartLamp(
+		bi2x, 1, panel_lights >> IIDX_IO_PANEL_LIGHT_P2_START & 1
+	);
+	aioIob2Bi2xTDJ_SetVefxButtonLamp(
+		bi2x, panel_lights >> IIDX_IO_PANEL_LIGHT_VEFX & 1
+	);
+	aioIob2Bi2xTDJ_SetEffectButtonLamp(
+		bi2x, panel_lights >> IIDX_IO_PANEL_LIGHT_EFFECT & 1
+	);
+
 	return;
 }
 
 /* Set state of the eight halogens above the marquee. */
 
 void iidx_io_ep1_set_top_lamps(uint8_t top_lamps) {
+	// TODO: Somehow map this to LM lights
 	return;
 }
 
 /* Switch the top neons on or off. */
 
 void iidx_io_ep1_set_top_neons(bool top_neons) {
+	// TODO: Also somehow map this to LM lights
 	return;
 }
 
@@ -53,7 +112,7 @@ void iidx_io_ep1_set_top_neons(bool top_neons) {
    IO error screen. */
 
 bool iidx_io_ep1_send(void) {
-	// TODO: UpdateDevicesStatus
+	aioNodeCtl_UpdateDevicesStatus();
 	return true;
 }
 
@@ -64,7 +123,7 @@ bool iidx_io_ep1_send(void) {
    IO error screen. */
 
 bool iidx_io_ep2_recv(void) {
-	// TODO: GetDeviceStatus
+	aioIob2Bi2xTDJ_GetDeviceStatus(bi2x, bi2x_inputBuffer, sizeof(bi2x_inputBuffer));
 	return true;
 }
 
