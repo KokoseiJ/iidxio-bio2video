de277ab30c5e94e0257655d212473eef3e984920
Wonjun Jung <kokoseij@gmail.com>
2025-04-09 02:26:28 -0700

    bio2test: add writefirm

--- src/main.c
+++ src/main.c
@@ -2,23 +2,33 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <conio.h>
 #include <windows.h>
 
 int main() {
 	uint8_t buffer[202] = {0,};
 
-	int i, j;
+	int i;
 
 	printf("[*] Initializing Bi2x...\n");
 
 	void *bi2x = init_bio2();
 	if (!bi2x) {
-		printf("[!] Failed to initialize bio2!");
+		printf("[!] Failed to initialize bio2!\n");
+		return 1;
+	}
+
+	printf("[*] Object created, trying writefirm...\n");
+
+	if (!write_firm()) {
+		printf("[!] Failed to write firmware!\n");
 		return 1;
 	}
 
 	printf("[*] Poll ready, press enter when you're ready:");
-	scanf("");
+	_getch();
+
+	
 
 	while (1) {
 		aioNodeCtl_UpdateDevicesStatus();
