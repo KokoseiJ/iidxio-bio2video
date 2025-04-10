5d4bdbf7c5764e76da5d9840151198166b9df6c9
Wonjun Jung <kokoseij@gmail.com>
2025-04-10 16:25:32 -0700

    libaio: add LED control functions

--- src/libaio-iob2_video.h
+++ src/libaio-iob2_video.h
@@ -19,5 +19,26 @@ void * __fastcall aioIob2Bi2xTDJ_SetStartLamp(
 );
 void * __fastcall aioIob2Bi2xTDJ_SetVefxButtonLamp(void *this, bool state);
 
+// Card reader LED
+void __fastcall aioIob2Bi2xTDJ_SetIccrLed(void *this, uint32_t playerSide, uint32_t colorValue);
+
+// Pillar LEDs (ws2812)?
+// sectionNum is seemingly used to decide which section to fill with?
+// sectionNum range 0~16 (17 sections)
+// ledData seems to be 24bit raw ws2812 data (MSB green, red, blue LSB)
+void __fastcall aioIob2Bi2xTDJ_SetTapeLedData(void *this, uint32_t sectionNum, void *ledData);
+
+void __fastcall aioIob2Bi2xTDJ_SetTurnTableLed(void *this, uint32_t playerSide, uint32_t colorValue);
+void * __fastcall aioIob2Bi2xTDJ_SetTurnTableResist(void *this, uint32_t playerSide, uint8_t resistance);
+
+void __fastcall aioIob2Bi2xTDJ_SetWooferLed(void *this, uint32_t colorValue);
+
 #endif
+
+/*
+TapeLed offset? (probably the address of the beginning of each section)
+.data:000000018002C280 rev_tapeled_offset_ dd 0, 13h, 10026h, 20053h, 30080h, 40095h, 400CBh, 400D6h
+.data:000000018002C2A0                     dd 400E1h, 50117h, 50128h, 60139h, 6017Dh, 601C1h, 701FEh
+.data:000000018002C2BC                     dd 70242h, 70286h, 802C3h
+*/
 
